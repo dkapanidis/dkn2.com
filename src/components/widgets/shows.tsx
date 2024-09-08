2b50670c70774aa3cb3f960532e864df325940b7
Dimitris Kapanidis <d.kapanidis@gmail.com>
2024-09-08 17:47:22 +0200

    add ratings

--- src/components/widgets/shows.tsx
+++ src/components/widgets/shows.tsx
@@ -15,10 +15,12 @@ import {
 import { useEffect, useState } from "react"
 import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from "recharts"
 import { Progress } from "@/components/ui/progress"
+import { HappyFace, NeutralFace, SadFace, SmilingFace } from "../ui/rating"
 
 type Show = {
     title: string
     url: string
+    rating: number
 }
 
 const getMonthKey = (date: Date): string => {
@@ -55,15 +57,13 @@ export function Shows() {
     const [traktData, setTraktData] = useState<Show[]>([])
 
     useEffect(() => {
-        fetchTraktData().then(data => setTraktData(data))
+        fetchTraktData().then(data => setTraktData(data.sort((a, b) => { 
+            if (b.rating == undefined) {
+                return -100;
+            }
+            return b.rating - a.rating
+         })))
     }, [])
-
-    const chartConfig = {
-        distance: {
-            label: "Distance",
-            color: "hsl(var(--chart-1))",
-        },
-    } satisfies ChartConfig
 
     return (
         <Card>
@@ -71,7 +71,7 @@ export function Shows() {
                 <CardTitle>Watching Shows</CardTitle>
             </CardHeader>
             <CardContent>
-                {traktData.filter((s) => s.url!=undefined).map((show, index) =>
+                {traktData.filter((s) => s.url != undefined).map((show, index) =>
                     <div key={index} className="flex items-center justify-between py-2">
                         <div className="flex items-center bg-gray-100 rounded-sm pr-2">
                             <img
@@ -84,9 +84,13 @@ export function Shows() {
                                     <div className="ml-2 flex-auto w-60">
                                         <p className="text-xs font-medium text-gray-600">{show.title}</p>
                                     </div>
-                                    {/* <div className="flex items-center">
-                                        <p className="text-xs">S04E01</p>
-                                    </div> */}
+
+                                    <div className="flex items-center w-7">
+                                        {show.rating <= 5 && <SadFace />}
+                                        {show.rating > 5 && show.rating <= 7 && <NeutralFace />}
+                                        {show.rating > 7 && show.rating <= 9 && <SmilingFace />}
+                                        {show.rating == 10 && <HappyFace />}
+                                    </div>
                                 </div>
                                 {/* <Progress
                                     value={0.4 * 100}
